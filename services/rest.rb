1544b963e45d239b38660e551566962aaa11b80d
Dan Stephenson <ispyhumanfly@gmail.com>
2017-10-30 16:34:26 -0400

    Updated the README more. I also added the Rest service stub.

---
+++ services/rest.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+class Web < EM::Connection
+    def initialize
+        super
+    end
+    def post_init
+    end
+    def receive_data
+    end
+    def unbind
+    end
+end
