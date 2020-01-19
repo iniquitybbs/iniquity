b96752b57edfd9d5b013e29c028c9edd3947045d
Dan Stephenson <danstephenson@Dans-Air.hsd1.mi.comcast.net>
2020-01-19 15:01:11 -0500

    Brought back iqlib.rb. It doesn't mean much right now but I do think there is value in keeping it.

---
+++ lib/iqlib.rb
@@ -0,0 +1,67 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+#-$a. ------------------ .a$ ---------------------------- %$!, ----------------%
+# `$¸   .%$$^¸$$aa.     .¸$`        .        .a$a$$.      `¸$%  $a$.        .
+#-.aaa$ $$$$'- $$$$$.- $aaa. -.a%$^"$$aa -- .$$$$$'- $$a. $aaa. `$,$ ----------%
+#;$$$$',a$a$  d$%$$$$$,'$$$$;$$$$$  $$$$$., $$%$$"  d$a$$ '$$$$; $$$   .a%$  $$a
+#:$$$$;$$$$%; Z$$$$$$$$;$$$$:$$$$$. $$$$^$,;$$&$$   Z$$$$,;$$$$.a$$$a..$$$   $$$
+#.$$$$ `$$$$.  $$$%$$$' $$$$.`$$$$  $$%$$$$ `$$$$.   $%$$$ $$$$""$$$" $$$$:  a$$
+# `$$$a.$$%$   $$$$$$';a$$$`  `¸$$aa$$$$&$': `$$$$a. $$$$'a$$$`.$$'$  $$$$;  $$$
+#%-----.------ $$$$'--------------- $$%$$' -- `¸$$$$$%$¸' ---- $$¸$a. `"$&$$#$%$
+#dz      .   .:'¸'     .        .   $$$$'     .        .       `¸$$$$y.     `$$&
+#%--------- a`-----------.--------- $$¸' -----.------------.---------------- $$$
+#   .      !a    . .    .      .   .:'   .  .                  .        .:.a$$$¸
+#.      .  '$a,          .        a` .   'a          .   .             s` .  . .
+#      .    ¸$Aa         .       !a       a!      .    .         ..   %s      .s
+#   .         ¸¸'     . .        '$$Aa.aA$$'        . .               `!$%a.a%#$
+#==============================================================================
+#   t h e    i n i q u i t y    b u l l e t i n    b o a r d    s y s t e m
+#==============================================================================
+
+require "yard"
+require "rdoc"
+
+# @api public
+# @api The Iniquity Model is not feature complete.
+#
+# @note A Comprehensive interface to the Iniquity Data Model
+#
+# @author Dan Stephenson 
+# @author Lawrence Manuel (smooth)
+
+module Iniquity
+
+    # @api users
+    class users
+        # @api public
+        #
+        # Create a new Iniquity user record.
+        #
+        # @param data [Symbol] the paramater format type, `:object` or `:json` or `:yaml`
+        # @return [Object] the object converted into the expected format.
+        def create(data = :object || :json)
+            # Do Something...
+        end
+        #
+        # @param data [Symbol] the format type, `:object` or `:json`
+        # @return [Object] the object converted into the expected format.
+
+        def read(data = :object || :json)
+        end
+        # Create a new BBS user.
+        #
+        # @param data [Symbol] the format type, `:object` or `:html`
+        # @return [Object] the object converted into the expected format.
+
+        def update(data = :object || :json)
+        end
+        # Update an existing BBS user.
+        #
+        # @param data [Symbol] the format type, `:object` or `:json`
+        # @return [Object] the object converted into the expected format.
+
+        def delete(data = :object)
+        end
+    end
+end
