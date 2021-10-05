c5e4acd488fdbc5951fd8bba68fc4b8b12e74d16
ispyhumanfly <ispyhumanfly@gmail.com>
2021-10-05 09:09:23 -0400

    🔥 Not ready for semantic releases just yet. So doing some cleanup.
    I'm sticking with lerna and npm workspaces for the time being.

--- packages/core/src/bbs.ts
+++ packages/core/src/bbs.ts
@@ -21,46 +21,44 @@ dz      .   .:'¸'     .        .   $$$$'     .        .       `¸$$$$y.     `$$
 ==============================================================================
 */
 
-const iq3bbs = new BBS({ basepath: "/iniquity/core/assets/" })
+const bbs = new BBS({ basepath: "/iniquity/core/assets/" })
 
-const welcomeArt = iq3bbs.artwork({ filename: files.sm_iniq2 })
+const welcomeArt = bbs.artwork({ filename: files.sm_iniq2 })
 welcomeArt.render({ clearScreenBefore: true, speed: 100 })
 
-iq3bbs
-    .print({
-        text: `You just connected to an iniquity bbs. The artwork you are seeing above is called ${welcomeArt.filename} It's still pretty new. Likely has bugs. Real talk, it's not even finished. But maybe you'll still think it's cool.`
-            .newlines()
-            .color("background red")
-            .center()
-    })
-    .pause({ colorReset: true, newlines: 2, center: true })
+bbs.print({
+    text: `You just connected to an iniquity bbs. The artwork you are seeing above is called ${welcomeArt.filename} It's still pretty new. Likely has bugs. Real talk, it's not even finished. But maybe you'll still think it's cool.`
+        .newlines()
+        .color("background red")
+        .center()
+}).pause({ colorReset: true, newlines: 2, center: true })
 
-iq3bbs.artwork({ filename: files.we_iniq3 }).render({ clearScreenBefore: false })
+bbs.artwork({ filename: files.we_iniq3 }).render({ clearScreenBefore: false })
 
-iq3bbs.say("You've connected to a prototype of the new iniquity BBS Development Platform.".newlines(2).color("bright red").center()).pause()
+bbs.say("You've connected to a prototype of the new iniquity BBS Development Platform.".newlines(2).color("bright red").center()).pause()
 
-iq3bbs.artwork({ filename: files.d_iniq1 }).render({ speed: 100 })
-const login = iq3bbs.ask("What is your login: ".newlines(1))
+bbs.artwork({ filename: files.d_iniq1 }).render({ speed: 100 })
+const login = bbs.ask("What is your login: ".newlines(1))
 switch (login) {
     case "new":
     case "signup":
-        iq3bbs.artwork({ filename: files.newuser1 }).render({ clearScreenBefore: true })
+        bbs.artwork({ filename: files.newuser1 }).render({ clearScreenBefore: true })
 
-        let newUser = iq3bbs.user({
-            name: iq3bbs.ask("What would you like your handle to be?".newlines(2).color("white")),
-            password: iq3bbs.ask("And your password?".newlines(2).color("white"))
+        let newUser = bbs.user({
+            name: bbs.ask("What would you like your handle to be?".newlines(2).color("white")),
+            password: bbs.ask("And your password?".newlines(2).color("white"))
         })
 
-        iq3bbs.say(`Welcome ${newUser.name}. And goodbye!`.newlines().center())
-        iq3bbs.disconnect()
+        bbs.say(`Welcome ${newUser.name}. And goodbye!`.newlines().center())
+        bbs.disconnect()
         break
     default:
-        if (iq3bbs.user({ name: login, password: iq3bbs.ask("And your password?".newlines(2).color("white")) })) {
+        if (bbs.user({ name: login, password: bbs.ask("And your password?".newlines(2).color("white")) })) {
             alert("somethingsync")
         }
 
-        iq3bbs.pause()
+        bbs.pause()
 
-        iq3bbs.artwork({ filename: files.d_iniq1 }).render({ clearScreenBefore: true })
+        bbs.artwork({ filename: files.d_iniq1 }).render({ clearScreenBefore: true })
         break
 }
