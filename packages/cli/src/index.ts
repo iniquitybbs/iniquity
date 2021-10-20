8d88dd9b31f4771a5dac0121035e7241d028e74a
ispyhumanfly <ispyhumanfly@gmail.com>
2021-10-20 02:38:25 -0400

    Hoping this fixes some of the cli issues.

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,5 +1,22 @@
+import * as path from "path"
 import yargs from "yargs"
-import * as Commands from "./commands"
 
-if (process.argv.length > 2) yargs.command(Commands.default).help().argv
-export default yargs.command(Commands.default).help().argv
+/**
+ * Cli
+ * @class
+ * @implements yargs.CommandModule
+ */
+class Cli implements yargs.CommandModule {
+    public command = "cli [command]"
+    public describe = "make a get HTTP request"
+    public builder = (yargs: yargs.Argv) => {
+        return yargs.commandDir(path.join(__dirname), { recurse: true, exclude: RegExp("/*.spec.*/") }).pkgConf("iniquity")
+    }
+    public handler(argv: yargs.Arguments) {}
+}
+
+const cmds: yargs.CommandModule = new Cli()
+
+if (process.argv.length > 2) yargs.command(cmds).help().argv
+
+export default cmds
