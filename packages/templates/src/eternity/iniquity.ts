04db60e97b832f03990e7b0ed9db23ae45ed6f4f
ispyhumanfly <ispyhumanfly@gmail.com>
2023-10-28 20:37:04 -0400

    feat a not so great but decent start to the eternity template.

--- packages/templates/src/eternity/iniquity.ts
+++ packages/templates/src/eternity/iniquity.ts
@@ -9,9 +9,16 @@ const iq = new IQ.Core()
 
 iq.basepath = "/dist/assets"
 
+iq.artwork().render({ filename: "as-ini.cp437.ans", mode: "@-codes", clearScreenBefore: false })
+iq.wait(2000)
+
 iq.say(`Welcome to ${config.name}.`).pause()
 
-iq.artwork().render({ filename: "4d-iniq1.ans" }).pause()
+iq.artwork().render({ filename: "newuser.cp437.ans", mode: "character", speed: 40 }).pause()
 
-iq.artwork().render({ filename: "newuser.ans", mode: "character", speed: 400 }).pause()
+iq.wait(3000)
+
+iq.say("Ok, logging off. Thanks for visiting!")
+iq.artwork().render({ filename: "4d-iniq1.ans", clearScreenBefore: false })
+iq.pause()
 
